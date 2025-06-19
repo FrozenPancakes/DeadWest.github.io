3ce9301605c0acd8856e1ab5467b1793a7487a34
FrozenPancakes <156951034+FrozenPancakes@users.noreply.github.com>
2025-06-19 02:23:18 +0000

    sss

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("silly!")
+from js import document, window
+
+console = document.getElementById("console")
+
+def log_key(event):
+    key = event.key
+    console.innerHTML += f"Key pressed: {key}\n"
+    console.scrollTop = console.scrollHeight
+
+# Attach the listener
+window.addEventListener("keydown", log_key)
